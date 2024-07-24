d0a10a1c229e97df89418b872be55131f40881e9
Gabriel Dressler <113690954+dresslert@users.noreply.github.com>
2024-07-23 21:25:55 -0300

    Add files via upload

---
+++ src/main/java/com/to_do/dresslert/DresslertApplication.java
@@ -0,0 +1,13 @@
+package com.to_do.dresslert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DresslertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DresslertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/to_do/dresslert/DresslertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.to_do.dresslert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DresslertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
